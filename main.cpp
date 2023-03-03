36ca7971ff1c3ffdffb33fa1c3ec439d36851f01
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2023-03-03 17:42:56 +0100

    Init bump map

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <vector>
-#include <cstddef>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "naagtro.h"
 
@@ -21,49 +22,105 @@ public:
     unsigned char* data();
 
 private:
+    void init();
+
     const int width_ = 320;
     const int height_ = 200;
     const int buffer_size_ = width_ * height_;
 
-    std::vector<unsigned char> buffer_;
+    unsigned char* buffer_;
 };
 
 DisplayController::DisplayController()
 {
-    set_mode13h();
-    buffer_.resize(static_cast<size_t>(buffer_size_));
+    init();
 }
 
 DisplayController::DisplayController(unsigned char* palette)
 {
-    set_mode13h();
+    init();
     set_palette(palette);
-
-    buffer_.resize(static_cast<size_t>(buffer_size_));
 }
 
 DisplayController::~DisplayController()
 {
     unset_mode13h();
+    if (buffer_ != nullptr) {
+        delete[] buffer_;
+    }
+}
+
+void DisplayController::init()
+{
+    set_mode13h();
+    buffer_ = new unsigned char[static_cast<size_t>(buffer_size_)];
 }
 
 void DisplayController::blur()
 {
-    do_blur(buffer_.data(), width_, height_);
+    do_blur(buffer_, width_, height_);
 }
 
 void DisplayController::flip()
 {
-    copy_buffer(buffer_.data());
+    copy_buffer(buffer_);
 }
 
 unsigned char* DisplayController::data()
 {
-    return buffer_.data();
+    return buffer_;
+}
+
+void init_bumpmap(unsigned char* bumpmap)
+{
+    unsigned char* ptr_bumpmap = bumpmap;
+    for (int i = 0; i < 32000 - 320 * 70 / 2; i++, ptr_bumpmap++) {
+        *ptr_bumpmap = 0;
+    }
+
+    //unpack NAAG logo
+    unsigned char* ptr_packed_logo = naaglogo;
+
+    for (int i = 32000 - 320 * 70 / 2; i < 320 * 70 / 8; i++, ptr_packed_logo++) {
+        for (int j = 7; j >= 0; j--, ptr_bumpmap++) {
+            if ((*ptr_packed_logo >> j) & 1) {
+                //make logo look convex
+                *ptr_bumpmap = 0;
+            }
+            else {
+                *ptr_bumpmap = 255;
+            }
+        }
+    }
+
+    for (int i = 0; i < 32000 - 320 * 70 / 2; i++, ptr_bumpmap++) {
+        *ptr_bumpmap = 0;
+    }
+
+    ptr_bumpmap = bumpmap;
+    for (int i = 0; i < 65536; i++, ptr_bumpmap++) {
+        if (*ptr_bumpmap != 0) {
+           *ptr_bumpmap = static_cast<unsigned char>(rand() & 255);
+        }
+    }
+
+    //blur several times
+    for (int i = 0; i < 4; i++) {
+        do_segment_blur(bumpmap);
+    }
+
+    //done initializing bump map
 }
 
 int main(void)
 {
+    //initialize RNG
+    srand(time(nullptr));
+
+    //initialize bump map
+    unsigned char* ptr_bumpmap = new unsigned char[65536];
+    init_bumpmap(ptr_bumpmap);
+
     char key;
     DisplayController dc(naagpal);
 
@@ -76,6 +133,11 @@ int main(void)
                 break;
             }
         } 
+    }
+
+    //clean up
+    if (ptr_bumpmap != nullptr) {
+        delete[] ptr_bumpmap;
     }
     
     return 0;
